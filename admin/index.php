90610fcde37ee20bfbc62b236f6dc35ddf172d65
Sirajum Munir <nezentbd@gmail.com>
2023-09-11 11:35:54 +0600

    Solved picture issue and added event registers

--- admin/index.php
+++ admin/index.php
@@ -44,7 +44,7 @@
 					    <div class="app-card app-card-stat shadow-sm h-100">
 						    <div class="app-card-body p-3 p-lg-4">
 							    <h4 class="stats-type mb-1">Departments</h4>
-							    <div class="stats-figure">03</div>
+							    <div class="stats-figure">09</div>
 								
 						    </div><!--//app-card-body-->
 						    <a class="app-card-link-mask" href="#"></a>
@@ -53,9 +53,9 @@
 				    <div class="col-6 col-lg-3">
 					    <div class="app-card app-card-stat shadow-sm h-100">
 						    <div class="app-card-body p-3 p-lg-4">
-							    <h4 class="stats-type mb-1">Posted Jobs</h4>
-							    <div class="stats-figure">23</div>
-								<!-- <?php echo $conn->query("SELECT * FROM careers")->num_rows; ?> -->
+							    <h4 class="stats-type mb-1">Registers</h4>
+							    <div class="stats-figure"><?php echo $conn->query("SELECT * FROM registers")->num_rows; ?></div>
+								
 							    <div class="stats-meta">
 								    Open</div>
 						    </div><!--//app-card-body-->
@@ -168,7 +168,7 @@
 					        <div class="app-card-header p-3">
 						        <div class="row justify-content-between align-items-center">
 							        <div class="col-auto">
-						                <h4 class="app-card-title">Stats List</h4>
+						                <h4 class="app-card-title">Event Registers List</h4>
 							        </div><!--//col-->
 							        <div class="col-auto">
 								        <div class="card-header-action">
@@ -182,47 +182,24 @@
 							        <table class="table table-borderless mb-0">
 										<thead>
 											<tr>
-												<th class="meta">Source</th>
-												<th class="meta stat-cell">Views</th>
-												<th class="meta stat-cell">Today</th>
+												<th class="meta">Name</th>
+												<th class="meta stat-cell">Email</th>
+												<th class="meta stat-cell">Department</th>
 											</tr>
 										</thead>
 										<tbody>
+										<?php
+											
+											$alumni = $conn->query("SELECT * FROM registers");
+											while($row = $alumni->fetch_assoc()):
+											?>
 											<tr>
-												<td><a href="#">google.com</a></td>
-												<td class="stat-cell">110</td>
-												<td class="stat-cell">
-													<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up text-success" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-									  <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-									</svg> 
-									                30%
-									            </td>
+												<td><a href="#"><?php echo $row['name'] ?></a></td>
+												<td class="stat-cell"><?php echo $row['email'] ?></td>
+												<td class="stat-cell"><?php echo $row['department'] ?></td>
 											</tr>
-											<tr>
-												<td><a href="#">getbootstrap.com</a></td>
-												<td class="stat-cell">67</td>
-												<td class="stat-cell">23%</td>
-											</tr>
-											<tr>
-												<td><a href="#">w3schools.com</a></td>
-												<td class="stat-cell">56</td>
-												<td class="stat-cell">
-													<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down text-danger" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-									  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
-									</svg>
-												    20%
-											    </td>
-											</tr>
-											<tr>
-												<td><a href="#">javascript.com </a></td>
-												<td class="stat-cell">24</td>
-												<td class="stat-cell">-</td>
-											</tr>
-											<tr>
-												<td><a href="#">github.com </a></td>
-												<td class="stat-cell">17</td>
-												<td class="stat-cell">15%</td>
-											</tr>
+											
+											<?php endwhile; ?>
 										</tbody>
 									</table>
 						        </div><!--//table-responsive-->
